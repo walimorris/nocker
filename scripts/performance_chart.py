481f6f3f89b784fcf3fb3bdc3abad9269e79069e
Wali Morris <walimmorris@gmail.com>
2025-12-27 04:36:32 -0800

    add requirements.txt to run performance charts

--- scripts/performance_chart.py
+++ scripts/performance_chart.py
@@ -6,6 +6,7 @@ import os
 # This script utilizes matplotlib to plot the performance of a given
 # Nocker portscan. It utilizes the durationMillis value reported by
 # the given task objects from a json report.
+# invocation ex: python performance_chart.py reports/scan_me_nmap_org_t100_c300.json reports/scan_me_nmap_org_t100_c300.png
 def plot_durations(input_file: str, output_file: str):
     if not os.path.isfile(input_file):
         raise FileNotFoundError(f"Input file does not exist: {input_file}")
